8d340ad40c5dbd4ac363aeefa07440fc530019b3
Vo Thanh Phuc (AS-ER/ESP3) <ThanhPhuc.Vo@bosch.com>
2023-04-01 21:32:20 +0200

    first commit

---
+++ src/main/java/com/springlearning/javagamingapp/JavaGamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.javagamingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGamingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlearning/javagamingapp/JavaGamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearning.javagamingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
